8b606582299b975a874971e76277dc424264e4d0
Ajay Patel <patel.ajay745@gmail.com>
2025-05-12 14:42:54 -0400

    added 404 page

--- src/main.tsx
+++ src/main.tsx
@@ -14,36 +14,34 @@ const Home = lazy(() => import("./pages/Home"));
 const About = lazy(() => import("./pages/About"));
 
 createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route
-            index
-            element={
-              <Suspense fallback={<p>Loading...</p>}>
-                <Home />
-              </Suspense>
-            }
-          />
-          <Route
-            path="about"
-            element={
-              <Suspense fallback={<p>Loading...</p>}>
-                <About />
-              </Suspense>
-            }
-          />
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route
+          index
+          element={
+            <Suspense fallback={<p>Loading...</p>}>
+              <Home />
+            </Suspense>
+          }
+        />
+        <Route
+          path="about"
+          element={
+            <Suspense fallback={<p>Loading...</p>}>
+              <About />
+            </Suspense>
+          }
+        />
 
-          <Route path="dashboard" element={<DashBoardLayout />}>
-            <Route index element={<DashBoardHome />}></Route>
-            <Route path="overview" element={<Overview />}></Route>
-            <Route path="setting" element={<Setting />}></Route>
-            <Route path="user/:id" element={<UserProfile />}></Route>
-          </Route>
+        <Route path="dashboard" element={<DashBoardLayout />}>
+          <Route index element={<DashBoardHome />}></Route>
+          <Route path="overview" element={<Overview />}></Route>
+          <Route path="setting" element={<Setting />}></Route>
+          <Route path="user/:id" element={<UserProfile />}></Route>
         </Route>
-        <Route path={"*"} element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
+      </Route>
+      <Route path={"*"} element={<NotFound />} />
+    </Routes>
+  </BrowserRouter>
 );
